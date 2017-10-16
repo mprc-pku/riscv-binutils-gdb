5243a05057110ff58dddbe8bcbd3f102ca6f2fed
Palmer Dabbelt <palmer@dabbelt.com>
2017-10-16 15:59:22 -0700

    RISC-V: Mark unsupported gas testcases
    There are individual comments that explain why each test isn't
    supported, but the vast majority of them are due to RISC-V's aggressive
    linker relaxation.  The SLEB test cases should eventually be supported,
    but the remaining ones probably won't ever be.
    
    2017-10-16  Palmer Dabbelt  <palmer@dabbelt.com>
    
            * testsuite/gas/all/align.d: Mark as unsupported on RISC-V.
            testsuite/gas/all/relax.d: Likewise.
            testsuite/gas/all/sleb128-2.d: Likewise.
            testsuite/gas/all/sleb128-4.d: Likewise.
            testsuite/gas/all/sleb128-5.d: Likewise.
            testsuite/gas/all/sleb128-7.d: Likewise.
            testsuite/gas/elf/section11.d: Likewise.
            testsuite/gas/all/gas.exp (diff1.s): Likewise.

--- gas/testsuite/gas/elf/section11.d
+++ gas/testsuite/gas/elf/section11.d
@@ -2,7 +2,9 @@
 #readelf: -S --wide
 #name: Disabling section padding
 # The RX port uses non standard section names.
-#skip: rx-*-*
+# RISC-V handles alignment via linker relaxation, so object files don't have
+# the expected alignment.
+#skip: rx-*-* riscv*-*-*
 
 #...
   \[ .\] .text[ 	]+PROGBITS[ 	]+0+00 0+[0-9a-f]+ 0+0(1|4|5) 00  AX  0   0 16
